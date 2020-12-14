948c52266527c344288f1716ea65e8aa9e904b4f
TheChrisKip <43065686+TheChrisKip@users.noreply.github.com>
2020-12-14 11:16:22 +0100

    Update start.sh
    minor bug fixes

--- start.sh
+++ start.sh
@@ -7,19 +7,20 @@
 # 2-12-2020 - ADDED - Merged all previous work into this script. This script now get the logs from the cluster through SCP.
 #		      The ELK stack gets deployed and the database is cleard and then filled with the logs.
 #		      After all the logs are uploaded to the elasticsearch database the kibana dasboard can be used on 'http://localhost:5601'
-# 8-12-2020 - ADDED - errors are being send to 'errorlog.txt' file.
+# 14-12-2020 - ADDED - minor bug fixes
+#
 
 clear
 mkdir SecurityLogs 2> errorlog.txt
 
-# Get the location the scan logs need to be transfered to.
-PWD=`pwd`
-logDestination="${PWD}/SecurityLogs/"
+# Get the location the scan logs need to be transfered to. Uncomment lines if you can access log destination with ssh.
+#PWD=`pwd`
+#logDestination="${PWD}/SecurityLogs/"
 
-read -p "Please enter the scp address of the cluster where the logs are generated (including path of the Securityscanlogs folder and all log files that you want to transfer): " scpAddress
+#read -p "Please enter the scp address of the cluster where the logs are generated (including path of the Securityscanlogs folder and all log files that you want to transfer): " scpAddress
 
 # Use SCP to transfer the logs from the Cluster to the local folder
-scp $scpAddress $logDestination &&
+#scp $scpAddress $logDestination &&
 
 # Start elastic in a seperate terminal window.
 gnome-terminal --title="Elasticsearch" -- ELK/elasticsearch-7.10.0/bin/elasticsearch
